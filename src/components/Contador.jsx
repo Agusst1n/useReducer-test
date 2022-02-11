cc14efdeff05f4c3fb3f5cb7980a5baac338fb90
Nitsuga <88418234+Agusst1n@users.noreply.github.com>
2022-02-10 21:40:10 -0300

    contador v2

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -12,22 +12,40 @@ function Contador() {
 //       }
 //   }
   
+
+  const TYPES={
+      INCREMENT:"INCREMENT",
+      INCREMENT_5:"INCREMENT_5",
+      DECREMENT:"DECREMENT",
+      DECREMENT_5:"DECREMENT_5",
+      RESET:"RESET"
+  }
+
   const initialState = {contador: 0}; //estado inicial 
   const [state, Dispatch] = useReducer(reducer,initialState);
 
   //creamos la funcion reducer
   function reducer (state,action){
       switch(action.type){
-          case "INCREMENT":
+          case TYPES.INCREMENT:
             return {contador:state.contador + 1}
-          case "DECREMENT":
+          case TYPES.INCREMENT_5:
+            return {contador:state.contador + action.payload}
+          case TYPES.DECREMENT:
             return {contador:state.contador - 1}
+          case TYPES.DECREMENT_5:
+            return {contador:state.contador - action.payload}
+          case TYPES.RESET:
+                return initialState
           default:
             return state; //esta funcion siempre retorna el estado
       }
   }
-  const sumaRedu = () =>Dispatch({type: "INCREMENT"});
-  const restaRedu = () =>Dispatch({type: "DECREMENT"});
+  const sumaRedu = () =>Dispatch({type: TYPES.INCREMENT});
+  const sumaRedu5 = () =>Dispatch({type: TYPES.INCREMENT_5, payload:5});
+  const restaRedu = () =>Dispatch({type: TYPES.DECREMENT});
+  const restaRedu5 = () =>Dispatch({type: TYPES.DECREMENT_5, payload:5});
+  const reset = () =>Dispatch({type: TYPES.RESET});
 
   return (
     <div style={{textAlign:"center"}}>
@@ -39,8 +57,11 @@ function Contador() {
         <br />
 
         <h1>Contador con useReducer</h1>
+        <button onClick={sumaRedu5}>+5</button>
         <button onClick={sumaRedu}>+</button>
+        <button onClick={reset}>0</button>
         <button onClick={restaRedu}>-</button>
+        <button onClick={restaRedu5}>-5</button>
         <h3>{state.contador}</h3>
     </div>
   )
